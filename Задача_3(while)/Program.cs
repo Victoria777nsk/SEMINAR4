d213fe2828e049e087ff4793ededc2381039a881
Victoria <victoriaerygina@yandex.ru>
2022-08-30 11:06:01 +0700

    Произведение чисел через цикл while

---
+++ Задача_3(while)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int result = 1;
+
+while (i <= number)
+{
+    result = result * i;
+    i++;
+}
+
+Console.Write($"Произведение цифр от 1 до {number} = {result}");
