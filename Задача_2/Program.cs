38056a7b5c998052c6c5e65a37b388c6cf6fa2ef
Victoria <victoriaerygina@yandex.ru>
2022-08-30 10:23:57 +0700

    Количество цифр в числе

---
+++ Задача_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberCount(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество цифр в числе {number} = {numberCount(number)}");
