c889115c90c3c887103c8e21951099c9f8161f88
Victoria <victoriaerygina@yandex.ru>
2022-08-30 10:39:41 +0700

    Произведение чисел от 1 до N

---
+++ Задача_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    int numberMultiply(int number)
+    {
+        int result = 1;
+        for (int i = 2; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+    Console.WriteLine($"Произведение цифр от 1 до {number} = {numberMultiply(number)}");
+}
+else 
+{
+    Console.WriteLine($"Ошибка! Число должно быть > 1 !");
+}
